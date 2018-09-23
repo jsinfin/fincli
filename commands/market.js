c55138b3e2eba819a1da38a860a8e371c1e806e5
Ahad Bokhari <ahadbokhari@gmail.com>
2018-09-23 16:05:05 -0400

    chore - basic market fetch and display tabular data. needs formatting

--- commands/market.js
+++ commands/market.js
@@ -2,9 +2,16 @@ const Table = require('cli-table2')
 const ora = require('ora')
 const moment = require('moment')
 const fetchIEXMarket = require('../utils/api')
+const chalk = require('chalk')
 
 module.exports = async (args) => {
   const spinner = ora().start()
+  const mic = args.mic || args.m
+  let marketData = null
+  let tapeATotal = 0
+  let tapeBTotal = 0
+  let tapeCTotal = 0
+
   const MarketTable = new Table({
     chars: {
       'mid': '',
@@ -14,22 +21,43 @@ module.exports = async (args) => {
     },
     head: [
       moment().format('MMMM Do YYYY'),
-      'TapeA',
-      'TapeB',
-      'TapeC',
-      'Total',
-      'Market Percent'
+      'TAPEA',
+      'TAPEB',
+      'TAPEC',
+      'TOTAL',
+      'MARKET PERCENT'
+    ]
+  })
+
+  const MarketTotalTable = new Table({
+    chars: {
+      'mid': '',
+      'left-mid': '',
+      'mid-mid': '',
+      'right-mid': ''
+    },
+    head: [
+      'TOTAL',
+      'TAPEA',
+      'TAPEB',
+      'TAPEC',
+      'TOTAL',
+      'MARKET PERCENT'
     ]
   })
 
   try {
-    const mic = args.mic || args.m
-    let marketData
-
     fetchIEXMarket()
       .then(response => {
-        console.log(response)
         marketData = response
+
+        for (let j=0; j < marketData.length; j++) {
+          tapeATotal += marketData[j].tapeA,
+          tapeBTotal += marketData[j].tapeB,
+          tapeCTotal += marketData[j].tapeC
+        }
+
+        console.log(tapeATotal)
 
         for (let i=0; i < marketData.length; i++) {
           MarketTable.push([
@@ -41,13 +69,20 @@ module.exports = async (args) => {
             marketData[i].marketPercent
           ])
         }
+
+        MarketTotalTable.push([
+          tapeATotal,
+          tapeBTotal,
+          tapeCTotal
+        ])
+
         console.log(MarketTable.toString())
+        console.log(MarketTotalTable.toString())
       })
 
     spinner.stop()
   } catch (err) {
     spinner.stop()
-
-    console.error(err)
+    console.log(err.message)
   }
 }
